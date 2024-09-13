22c2d751dd858442590146ffe658fe86ba497dda
Prakhar Pratap Singh <123646107+prakhar5100@users.noreply.github.com>
2024-09-13 22:50:53 +0530

    Update SignUp.jsx

--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -1,51 +1,69 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import Confetti from "react-confetti";
 
 const SignUp = ({ onDelete, next }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [rememberMe, setRememberMe] = useState(false);
   const [isVisible, setIsVisible] = useState(true);
+  const [isConfettiRunning, setIsConfettiRunning] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsConfettiRunning(false);
+      setIsVisible(false); // Stop confetti after 5 seconds
+    }, 15000); // 5000ms = 5 seconds
+
+    // Cleanup the timer
+    return () => clearTimeout(timer);
+  }, []);
 
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://127.0.0.1:8000/api/authentication/register/', {
-        email: email,
+
+      const response = await axios.post('/api/authentication/register/', {
+        username: email, // Assuming the backend expects 'username' instead of 'email'
         password
       });
-      console.log('Sign Up successful!', response.data);
-      setIsVisible(false);
+      setIsConfettiRunning(true);
+      alert('Sign Up successful!', response.data);
+
+      // Hide the login section after successful submission
+      // Here you can add logic to handle successful login (e.g., storing tokens, redirecting)
     } catch (error) {
-      console.error('Error sign up', error);
+      alert('Error sign up');
+      // Here you can add logic to handle login errors (e.g., showing error messages)
     }
   };
 
   if (!isVisible) {
-    return null;
+    return null; // Render nothing if the component is not visible
   }
 
   return (
-    <section className='bg-black bg-opacity-35 absolute inset-0 h-screen'>
+    <section className='bg-black bg-opacity-35 absolute inset-0 h-screen font '>
+            {isConfettiRunning && <Confetti />}
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+        <div className="w-full rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 bg-[#e2e8f0]">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 className="flex justify-between text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+            <h1 className="flex justify-between text-xl font-bold leading-tight tracking-tight text-[#263246] md:text-2xl">
               Sign up to your account
               <div onClick={onDelete}>
-                <svg className="cursor-pointer w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                <svg className="cursor-pointer w-6 h-6 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                   <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18 17.94 6M18 18 6.06 6" />
                 </svg>
               </div>
             </h1>
             <form className="space-y-4 md:space-y-6" onSubmit={handleLogin}>
               <div>
-                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 ">Your email</label>
                 <input
                   type="email"
                   name="email"
                   id="email"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="bg-gray-50 border border-gray-300 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500 text-white"
                   placeholder="name@company.com"
                   required
                   value={email}
@@ -53,7 +71,7 @@ const SignUp = ({ onDelete, next }) => {
                 />
               </div>
               <div>
-                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">Password</label>
                 <input
                   type="password"
                   name="password"
@@ -78,12 +96,12 @@ const SignUp = ({ onDelete, next }) => {
                     />
                   </div>
                   <div className="ml-3 text-sm">
-                    <label htmlFor="remember" className="text-gray-500 dark:text-gray-300">Remember me</label>
+                    <label htmlFor="remember" className="text-[#263246]">Remember me</label>
                   </div>
                 </div>
               </div>
               <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign Up</button>
-              <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+              <p className="text-sm font-light text-[#263246]">
                 Already have an account? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500" onClick={next}>Sign In</a>
               </p>
             </form>
